d45a091ddc21fa08f5f292d92d6919b4f4b83262
Onur Onarıcı <onur.onarici@stm.com.tr>
2025-04-22 14:36:14 +0300

    defaultArgument.cpp

--- NC_Notes/3_/defaultArgument.cpp
+++ NC_Notes/3_/defaultArgument.cpp
@@ -23,9 +23,10 @@ int main()
 */
 
 //-------------------------------------------------------------------
-
+/*
 #include <iostream>
 
+//there is no extra cost for running time using default argument
 void func(int = 10, int = 20, int = 30);
 
 int main()
@@ -36,7 +37,36 @@ int main()
     func();
 }
 
+//Default argument should be in decleration, if there is decleration. If there is just definition, it could be default argument.
 void func(int x, int y, int z)
 {
     std::cout << "x = " << x << " y = " << y << " z = " << z << "\n";
 }
+*/
+
+//---------------------------------------------------------------
+
+//>>>>>>>>>>>> function re-decleration <<<<<<<<<<<<
+
+/*
+#include "asd.h"
+
+void foo(int, int, int = 10); //assume that its a function which is declared in asd.h header file and we can re-declera it without changing number of function parameter
+                              //compiler evaluate it as cumulative.
+int main(){ foo(1, 3); }
+*/
+
+//using just first argument as a syntax error, how can we do it? By wrapping the function
+/*
+#include "asd.h"
+
+void foo(int x, int y, int y);
+
+void call_foo(int y, int z, int x = 10)
+{
+    foo(x, y, z);
+}
+
+
+int main(){ call_foo(1, 3); }
+*/
